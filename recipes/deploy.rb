795bbdca034287bfcb9c4da6196c37737b72e55a
John Hwang <johnyhwang@gmail.com>
2012-03-08 08:57:45 -0500

    Need to rely on default deploy task

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,18 +1,18 @@
 namespace :deploy do
-  desc "Default deploy task: updates code, disables web, updates symlinks, backup db, backup assets, migrates, restarts web server, enables web, cleans up old releases"
-  task :default do
-    transaction do
-      update_code
-      web.disable
-      symlink
-      backup.create if backup_on_deploy
-      migrate
-    end
-
-    passenger.restart
-    web.enable
-    cleanup
-  end
+  # desc "Default deploy task: updates code, disables web, updates symlinks, backup db, backup assets, migrates, restarts web server, enables web, cleans up old releases"
+  # task :default do
+  #   transaction do
+  #     update_code
+  #     web.disable
+  #     symlink
+  #     backup.create if backup_on_deploy
+  #     migrate
+  #   end
+  # 
+  #   passenger.restart
+  #   web.enable
+  #   cleanup
+  # end
   
   desc "Symlinks config/database.yml and all entries in symlinks from \#{shared_path}/\#{symlink} to \#{release_path}/\#{symlink}."
   task :symlink_extras, :except => { :no_release => true } do
